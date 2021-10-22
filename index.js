6cda51408b3b02d3104f4dc95620391d7cf83602
pritam-dey-19 <dey.pritam19@gmail.com>
2021-10-22 14:56:48 +0530

    initail commit

---
+++ index.js
@@ -0,0 +1,105 @@
+// weather app js file
+
+let weather = {
+  apiKey: "ae4598498d62a1fb1eb5c8ad73c1edc7",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const dateInfo = getDateInformation();
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    const { visibility } = data;
+
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".date").innerText =
+      dateInfo.date +
+      ", " +
+      dateInfo.month +
+      " " +
+      dateInfo.year +
+      " - " +
+      dateInfo.day;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".visibility").innerText =
+      "Visibility: " + visibility;
+    document.querySelector(".avg").innerText = "Avg.Temp: " + temp + "°C";
+    document.querySelector(".weather").classList.remove("loading");
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+function getDateInformation() {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const d = new Date();
+  const day = days[d.getDay()];
+  const date = d.getDate();
+  const month = months[d.getMonth()];
+  const year = d.getFullYear();
+  return {
+    day,
+    date,
+    month,
+    year,
+  };
+}
+
+weather.fetchWeather("Kolkata");
